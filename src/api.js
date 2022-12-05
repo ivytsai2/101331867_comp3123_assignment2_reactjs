0d248e8e8901e5dae2b9baf3073e780ed7628768
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2022-12-04 19:30:44 -0500

    change baseURL to herokuapp

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'https://comp3123-assignment1-101331867.herokuapp.com/api',
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
